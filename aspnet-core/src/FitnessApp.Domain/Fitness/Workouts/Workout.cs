de3fdb6b8c1d9e9300050cc57135c84b08794921
Enes Ismet Bal <148435322+enisba@users.noreply.github.com>
2025-09-03 17:19:40 +0300

    Add Workout and WorkoutExercise domain entities
    Introduced Workout and WorkoutExercise classes to represent workout sessions and their associated exercises. These entities include properties and methods for managing workout details and exercise assignments, forming the foundation for workout management in the domain layer.

---
+++ aspnet-core/src/FitnessApp.Domain/Fitness/Workouts/Workout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace FitnessApp.Fitness.Workouts
+{
+    public class Workout : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; private set; }
+        public Guid UserId { get; private set; } 
+
+        public ICollection<WorkoutExercise> Exercises { get; private set; }
+
+        protected Workout() { }
+
+        public Workout(Guid id, string name, Guid userId) : base(id)
+        {
+            Name = name;
+            UserId = userId;
+            Exercises = new List<WorkoutExercise>();
+        }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ aspnet-core/src/FitnessApp.Domain/Fitness/Workouts/WorkoutExercise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace FitnessApp.Fitness.Workouts
+{
+    public class WorkoutExercise : Entity<Guid>
+    {
+        public Guid WorkoutId { get; private set; }
+        public Guid ExerciseId { get; private set; }
+
+        public int Sets { get; private set; }
+        public int Reps { get; private set; }
+        public double? Weight { get; private set; } 
+
+        protected WorkoutExercise() { }
+
+        public WorkoutExercise(Guid id, Guid workoutId, Guid exerciseId, int sets, int reps, double? weight = null)
+            : base(id)
+        {
+            WorkoutId = workoutId;
+            ExerciseId = exerciseId;
+            Sets = sets;
+            Reps = reps;
+            Weight = weight;
+        }
+
+        public void UpdateDetails(int sets, int reps, double? weight = null)
+        {
+            Sets = sets;
+            Reps = reps;
+            Weight = weight;
+        }
+    }
+}
