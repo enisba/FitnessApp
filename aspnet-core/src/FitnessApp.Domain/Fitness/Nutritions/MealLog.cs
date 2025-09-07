d9a77cd699d707a180e0b7e4234eb343e9a9ae8a
Enes Ismet Bal <148435322+enisba@users.noreply.github.com>
2025-09-07 20:27:31 +0300

    Add MealLog and MealLogItem domain entities
    Introduces MealLog and MealLogItem classes to represent user meal logs and their items. These entities will be used to track meals, meal types, and quantities for nutrition logging in the application.

---
+++ aspnet-core/src/FitnessApp.Domain/Fitness/Nutritions/MealLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Guids;
+
+namespace FitnessApp.Fitness.Nutritions
+{
+    public class MealLog : FullAuditedAggregateRoot<Guid>
+    {
+        public Guid UserId { get; private set; }
+        public DateTime Date { get; private set; }
+        public string MealType { get; private set; } // Kahvaltı, Öğle, Akşam, Ara öğün
+
+        public ICollection<MealLogItem> Items { get; private set; }
+
+        protected MealLog() { }
+
+        public MealLog(Guid id, Guid userId, DateTime date, string mealType) : base(id)
+        {
+            UserId = userId;
+            Date = date.Date;
+            MealType = mealType;
+            Items = new List<MealLogItem>();
+        }
+
+        public void AddMeal(Guid mealId, int quantity)
+        {
+            Items.Add(new MealLogItem(Guid.NewGuid(), Id, mealId, quantity));
+        }
+
+    }
+}
---
+++ aspnet-core/src/FitnessApp.Domain/Fitness/Nutritions/MealLogItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace FitnessApp.Fitness.Nutritions
+{
+    public class MealLogItem : Entity<Guid>
+    {
+        public Guid MealLogId { get; private set; }
+        public Guid MealId { get; private set; }
+        public int Quantity { get; private set; } // 1 porsiyon, 2 porsiyon...
+
+        protected MealLogItem() { }
+
+        public MealLogItem(Guid id, Guid mealLogId, Guid mealId, int quantity) : base(id)
+        {
+            MealLogId = mealLogId;
+            MealId = mealId;
+            Quantity = quantity;
+        }
+    }
+}
